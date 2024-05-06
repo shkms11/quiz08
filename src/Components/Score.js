00a0e29b5e0350144149a388a5b2cd29b0fabd59
Shamsul <48136141+shkms11@users.noreply.github.com>
2024-05-06 08:18:53 +0600

    Update Score.js

---
+++ src/Components/Score.js
@@ -0,0 +1,15 @@
+import React, {Component} from'react';
+import '../App.css'
+
+class Score extends Component {
+    render() {
+        const {score, onNextQuestion} = this.props;
+
+        return (
+            <div>
+                <h2>Results</h2>
+                <h4>Your score: {score}</h4>
+            </div>
+        )
+    }
+}
